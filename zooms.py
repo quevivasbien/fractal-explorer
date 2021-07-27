7cba405e52022a05056358ed4e78b05379b02469
quevivasbien <jensenm@uchicago.edu>
2021-07-27 12:06:53 -0500

    gave up on fancy panning algorithm

--- zooms.py
+++ zooms.py
@@ -17,8 +17,6 @@ from scipy.interpolate import interp1d
 
 from mandelbrot import MandelbrotMakerGPU
 
-import matplotlib.pyplot as plt
-
 # def bernstein_poly(k, n, t):
 #     return comb(n, k) * t**k * (1-t)**(n-k)
 
@@ -41,12 +39,93 @@ def smooth_curve(x, y, resolution=100):
     yout = interp1d(t_, y, kind='cubic')(t)
     return xout, yout
 
+# # This whole thing doesn't work. It creates artifacts from stretching... :(
+# # I need to be able to do sub-pixel shifting if I want this to work
+# def recycle_frame(last_frame, last_xstart, last_xstop, last_ystart, last_ystop,
+#                   xstart, xstop, ystart, ystop, mm):
+#     last_width = last_xstop - last_xstart
+#     last_height = last_ystop - last_ystart
+#     if xstart > last_xstart:
+#         xmove = 0  # move right
+#         lost_width = xstart - last_xstart
+#         lost_pixelwidth = int(last_frame.shape[1] * lost_width / last_width)
+#         keep = last_frame[:, lost_pixelwidth:]
+#         vert_xstart, vert_xstop = last_xstop, xstop
+#     else:
+#         xmove = 1  # move right
+#         lost_width = last_xstart - xstart
+#         lost_pixelwidth = int(last_frame.shape[1] * lost_width / last_width)
+#         keep = last_frame[:, :-lost_pixelwidth]
+#         vert_xstart, vert_xstop = xstart, last_xstart
+#     if ystart > last_ystart:
+#         ymove = 0  # move down
+#         lost_height = ystart - last_ystart
+#         lost_pixelheight = int(last_frame.shape[1] * lost_height / last_height)
+#         keep = keep[lost_pixelheight:, :]
+#         horiz_ystart, horiz_ystop = last_ystop, ystop
+#     else:
+#         ymove = 1  # move up
+#         lost_height = last_ystart - ystart
+#         lost_pixelheight = int(last_frame.shape[1] * lost_height / last_height)
+#         keep = keep[:-lost_pixelheight, ]
+#         horiz_ystart, horiz_ystop = ystart, last_ystart
+#     assert lost_pixelwidth >= 0
+#     assert lost_pixelheight >= 0
+#     # assemble new vertical component
+#     pxwidth = last_frame.shape[1] - keep.shape[1]
+#     if pxwidth > 0:
+#         # create vertical bar that goes to right or left of kept section
+#         vert = mm.gen_mandelbrot(vert_xstart, vert_xstop,
+#                                  ystart, ystop,
+#                                  pixelwidth=pxwidth,
+#                                  pixelheight=last_frame.shape[0])
+#     else:
+#         vert = None
+#     # assemble new horizontal component
+#     pxheight = last_frame.shape[0] - keep.shape[0]
+#     if pxheight > 0:
+#         if xmove == 0:
+#             horiz = mm.gen_mandelbrot(xstart, vert_xstart,
+#                                       horiz_ystart, horiz_ystop,
+#                                       pixelwidth=keep.shape[1],
+#                                       pixelheight=pxheight)
+#         else:
+#             horiz = mm.gen_mandelbrot(vert_xstop, xstop,
+#                                       horiz_ystart, horiz_ystop,
+#                                       pixelwidth=keep.shape[1],
+#                                       pixelheight=pxheight)
+#     else:
+#         horiz = None
+#     # now put everything together
+#     if vert is not None and horiz is not None:
+#         if xmove == 0 and ymove == 1:
+#             f = np.concatenate((np.concatenate((horiz, keep), 0), vert), 1)
+#         elif xmove == 0 and ymove == 0:
+#             f = np.concatenate((np.concatenate((keep, horiz), 0), vert), 1)
+#         elif xmove == 1 and ymove == 1:
+#             f = np.concatenate((vert, np.concatenate((horiz, keep), 0)), 1)
+#         else:
+#             f = np.concatenate((vert, np.concatenate((keep, horiz), 0)), 1)
+#     # Deal with cases where we don't need one of the slices
+#     elif vert is None and horiz is None:
+#         f = keep
+#     elif vert is None:
+#         if ymove == 0:
+#             f = np.concatenate((keep, horiz), 0)
+#         else:
+#             f = np.concatenate((horiz, keep), 0)
+#     else:
+#         if xmove == 0:
+#             f = np.concatenate((keep, vert), 1)
+#         else:
+#             f = np.concatenate((vert, keep), 1)
+#     return f
 
-def gen_path_from_coords(pixelwidth=512, nframes=100,
+
+def gen_path_from_coords(pixelwidth=512, pixelheight=None, nframes=100,
                          coord_source='coordinates.txt',
                          save_loc='~/Documents/python/', keep_frames=False,
                          quality=10, fps=30, buffer_frames=30, colors=None):
-    """Doesn't currently work properly"""
     # import and process the list of coordinates
     save_loc = os.path.expanduser(save_loc)
     if not os.path.isdir(save_loc):
@@ -55,22 +134,14 @@ def gen_path_from_coords(pixelwidth=512, nframes=100,
         coords = fh.read().strip().split('\n')
     searches = [re.search(r'^re (-?[\d\.]+)_(-?[\d\.]+) im (-?[\d\.]+)_(-?[\d\.]+)$', x) \
                 for x in coords]
-    xstarts = [float(s.group(1)) for s in searches]
-    xstops = [float(s.group(2)) for s in searches]
-    ystarts = [float(s.group(3)) for s in searches]
-    ystops = [float(s.group(4)) for s in searches]
-    # figure out path
-    width = xstops[0] - xstarts[0]
-    height = ystops[0] - ystarts[0]
+    xstarts = np.array([float(s.group(1)) for s in searches])
+    xstops = np.array([float(s.group(2)) for s in searches])
+    ystarts = np.array([float(s.group(3)) for s in searches])
+    ystops = np.array([float(s.group(4)) for s in searches])
     xstart_path, ystart_path = smooth_curve(xstarts, ystarts, nframes)
-    xstop_path, ystop_path = xstart_path + width, ystart_path + height
-    plt.plot(xstart_path, ystart_path)
-    plt.scatter(xstarts, ystarts)
-    plt.plot(xstop_path, ystop_path)
-    plt.scatter(xstops, ystops)
-    plt.show()
-    return
-    pixelheight = int(pixelwidth * height / width)
+    xstop_path, ystop_path = smooth_curve(xstops, ystops, nframes)
+    if pixelheight is None:
+        pixelheight = int(pixelwidth * (ystops[0] - ystarts[0]) / (xstops[0] - xstarts[0]))
     if colors is None:
         mm = MandelbrotMakerGPU()
     else:
@@ -78,89 +149,9 @@ def gen_path_from_coords(pixelwidth=512, nframes=100,
                                 c3=colors[3], c4=colors[4])
     frames = []
     for i in range(0, nframes):
-        if i == 0:
-            f= mm.gen_mandelbrot(xstart_path[i], xstop_path[i],
-                                 ystart_path[i], ystop_path[i],
-                                 pixelwidth=pixelwidth, pixelheight=pixelheight)
-        else:
-            # recycle the last frame
-            # what a mess;
-            # I should probably put this in its own function and comment heavily
-            # TODO: deal with zero or negative matrix sizes
-            last = frames[i-1]
-            last_width = xstop_path[i-1] - xstart_path[i-1]
-            last_height = ystop_path[i-1] - ystart_path[i-1]
-            if xstart_path[i] > xstart_path[i-1]:  # move to right
-                xmove = 0
-                lost_width = xstart_path[i] - xstart_path[i-1]
-                lost_pixelwidth = int(last.shape[1] * lost_width / last_width)
-                keep = last[:, lost_pixelwidth:]
-                xstart, xstop = xstop_path[i-1], xstop_path[i]
-            else:  # move left
-                xmove = 1
-                shared_width = xstop_path[i] - xstart_path[i-1]
-                shared_pixelwidth = int(last.shape[1] * shared_width / last_width)
-                keep = last[:, :shared_pixelwidth]
-                xstart, xstop = xstart_path[i], xstart_path[i-1]
-            if ystart_path[i] > ystart_path[i-1]:  # move down
-                ymove = 0
-                lost_height = ystart_path[i] - ystart_path[i-1]
-                lost_pixelheight = int(last.shape[0] * lost_height / last_height)
-                keep = keep[lost_pixelheight:, :]
-                ystart, ystop = ystop_path[i-1], ystop_path[i]
-            else:  # move up
-                ymove = 1
-                shared_height = ystop_path[i] - ystart_path[i-1]
-                shared_pixelheight = int(last.shape[0] * shared_height / last_height)
-                keep = keep[:shared_pixelheight, :]
-                ystart, ystop = ystart_path[i], ystart_path[i-1]
-            # assemble new vertical component
-            pxwidth = last.shape[1] - keep.shape[1]
-            if pxwidth > 0:
-                vert = mm.gen_mandelbrot(xstart, xstop,
-                                         ystart_path[i], ystop_path[i],
-                                         pixelwidth=pxwidth,
-                                         pixelheight=last.shape[0])
-            else:
-                vert = None
-            # assemble new horizontal component
-            pxheight = last.shape[0] - keep.shape[0]
-            if pxheight > 0:
-                if xmove == 0:
-                    horiz = mm.gen_mandelbrot(xstart_path[i], xstart,
-                                              ystart, ystop,
-                                              pixelwidth=keep.shape[1],
-                                              pixelheight=pxheight)
-                else:
-                    horiz = mm.gen_mandelbrot(xstop, xstop_path[i],
-                                              ystart, ystop,
-                                              pixelwidth=keep.shape[1],
-                                              pixelheight=pxheight)
-            else:
-                horiz = None
-            # now put everything together
-            if vert is not None and horiz is not None:
-                if xmove == 0 and ymove == 1:
-                    f = np.concatenate((np.concatenate((horiz, keep), 0), vert), 1)
-                elif xmove == 0 and ymove == 0:
-                    f = np.concatenate((np.concatenate((keep, horiz), 0), vert), 1)
-                elif xmove == 1 and ymove == 1:
-                    f = np.concatenate((vert, np.concatenate((horiz, keep), 0)), 1)
-                else:
-                    f = np.concatenate((vert, np.concatenate((keep, horiz), 0)), 1)
-            # Deal with cases where we don't need one of the slices
-            elif vert is None and horiz is None:
-                f = keep
-            elif vert is None:
-                if ymove == 0:
-                    f = np.concatenate((keep, horiz), 0)
-                else:
-                    f = np.concatenate((horiz, keep), 0)
-            else:
-                if xmove == 0:
-                    f = np.concatenate((keep, vert), 1)
-                else:
-                    f = np.concatenate((vert, keep), 1)
+        f= mm.gen_mandelbrot(xstart_path[i], xstop_path[i],
+                             ystart_path[i], ystop_path[i],
+                             pixelwidth=pixelwidth, pixelheight=pixelheight)
         # save frame
         if keep_frames:
             if not os.path.isdir(os.path.join(save_loc, 'frames')):
@@ -173,4 +164,3 @@ def gen_path_from_coords(pixelwidth=512, nframes=100,
     frames = [frames[0]]*buffer_frames + frames + [frames[-1]]*buffer_frames
     imageio.mimwrite(os.path.join(save_loc, 'bezier_path.mp4'),
                      frames, fps=fps, quality=quality)
-    
